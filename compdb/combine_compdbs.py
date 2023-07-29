1ed88fa2f6cb8f88fdf8b0eeb9b60f2fd7afb12d
Andy Friesen <andy.friesen@gmail.com>
2023-07-29 12:23:24 -0700

    This works, but it's pretty goofy.

---
+++ compdb/combine_compdbs.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+
+import sys
+import argparse
+from typing import List
+import json
+
+parser = argparse.ArgumentParser()
+
+parser.add_argument("database", nargs='+')
+parser.add_argument('--output', '-o', dest='output', required=True)
+
+def main(argv: List[str]):
+    args = parser.parse_args(argv)
+
+    result = {}
+
+    for fn in args.database:
+        content = json.load(open(fn, 'r'))
+
+        for row in content:
+            result[row['file']] = row
+
+    res = list(result.values())
+
+    json.dump(res, open(args.output, 'w'), indent = 4)
+
+if __name__ == '__main__':
+    main(sys.argv[1:])
